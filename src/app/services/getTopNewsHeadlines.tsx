4d33ccb74ddcb07295f7b48ae6d33b4b6c101787
Abhilakshya Dobhal <abhilakshyadobhal@gmail.com>
2020-05-30 00:16:24 +0530

    removed https

--- src/app/services/getTopNewsHeadlines.tsx
+++ src/app/services/getTopNewsHeadlines.tsx
@@ -4,7 +4,7 @@ export const getTopNewHeadLines = async (_params: any) => {
   const params = {
     ..._params,
   };
-  const res = await axios.get('https://newsapi.org/v2/top-headlines', {
+  const res = await axios.get('http://newsapi.org/v2/top-headlines', {
     params,
     headers: {
       Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
