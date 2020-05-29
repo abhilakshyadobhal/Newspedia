742d494de8634d49b6126ccb82e16680355c6db9
Abhilakshya Dobhal <abhilakshyadobhal@gmail.com>
2020-05-30 00:08:25 +0530

    added auth in headers

--- src/app/services/getTopNewsHeadlines.tsx
+++ src/app/services/getTopNewsHeadlines.tsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 export const getTopNewHeadLines = async (_params: any) => {
   const params = {
     ..._params,
-    apiKey: process.env.REACT_APP_API_KEY,
   };
   const res = await axios.get('https://newsapi.org/v2/top-headlines', {
     params,
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+    },
   });
   return res.data;
 };
